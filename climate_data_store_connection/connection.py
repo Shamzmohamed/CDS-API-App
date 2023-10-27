a14a6def162fd18dd63fdc206653885c20fcdfa4
Shamzmohamed <m20210707@novaims.unl.pt>
2023-10-27 22:05:11 +0200

    data_path added

--- climate_data_store_connection/connection.py
+++ climate_data_store_connection/connection.py
@@ -13,15 +13,14 @@ class ClimateDataStoreConnection(ExperimentalBaseConnection[cdsapi.api.Client]):
     def retrieve(self) -> cdsapi.api.Client.retrieve:
         return self._instance.retrieve
     
-    def query(self, query_param,output_file):
+    def query(self, query_param,data_path):
 
         query_param['product_type'] = 'reanalysis'
         query_param['format'] = 'netcdf'
 
         # adding time in file name to avoid overlapping
         data_path = 'dd_'+str(datetime.now()).replace(":","-")+'.nc'
-
         ret = self.retrieve()
-
         ret('reanalysis-era5-single-levels',query_param,data_path)
+        return data_path
         
