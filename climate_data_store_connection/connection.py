8bdf2f817fc11aa7d81cc537ddac76b85580d67b
Shamzmohamed <m20210707@novaims.unl.pt>
2023-10-27 23:00:46 +0200

    file path added

--- climate_data_store_connection/connection.py
+++ climate_data_store_connection/connection.py
@@ -1,5 +1,6 @@
 from streamlit.connections import ExperimentalBaseConnection
 import cdsapi
+import shutil
 import pandas as pd
 import xarray as xr
 import os
@@ -13,14 +14,19 @@ class ClimateDataStoreConnection(ExperimentalBaseConnection[cdsapi.api.Client]):
     def retrieve(self) -> cdsapi.api.Client.retrieve:
         return self._instance.retrieve
     
-    def query(self, query_param,data_path):
+    def query(self, query_param,down):
 
         query_param['product_type'] = 'reanalysis'
         query_param['format'] = 'netcdf'
+        down = '/home/mohamed/Documents/CDS-API-Connect/CDS-API-App/data'
 
         # adding time in file name to avoid overlapping
         data_path = 'dd_'+str(datetime.now()).replace(":","-")+'.nc'
+        local_path = os.path.join(down, data_path)
         ret = self.retrieve()
-        ret('reanalysis-era5-single-levels',query_param,data_path)
-        return data_path
+        ret('reanalysis-era5-single-levels',query_param,local_path)
+        # final_path = os.path.join(down, data_path)
+        # shutil.move(local_path, final_path)
+        
+        return local_path
         
