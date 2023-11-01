db78d12794a1b502dd5cf9fc39e84157dbba8e37
Shamzmohamed <m20210707@novaims.unl.pt>
2023-11-01 21:18:42 +0100

    interact map

--- intmap.py
+++ intmap.py
@@ -1,54 +1,87 @@
-# Data is visualized as a plotly (interactive map) - Working
-from datetime import datetime, timedelta
-from netCDF4 import Dataset as NetCDFFile
-import plotly.graph_objects as go
+import urllib.request
 import streamlit as st
+from datetime import datetime, timedelta
+import folium
+from folium import plugins
+from streamlit_folium import folium_static as st_folium
 import pandas as pd
+import xarray as xr
+import numpy as np
+from cdsapi import Client
 
-@st.cache_data
-def get_data():
-    filename = "/home/mohamed/Documents/CDS-API-Connect/CDS-API-App/data/dd_2023-10-31 11-55-39.444854.nc"
-    return filename
+st.set_page_config(page_title="CDS API", page_icon="🌤️")
+st.title("ClimateDataStoreConnection")
 
-filename = get_data()
-nc = NetCDFFile(filename)
-title = getattr(nc, 'title', 'Title Not Found')
+# Function to download data from CDS API
+def download_data(year, variable):
+    c = Client()
 
-# Extract data from the NetCDF file
-st.title(title)  # Display the title, even if it's not found
-st.header("Description")
-description = getattr(nc, 'description', 'Description Not Found')
-st.write(description)  # Display the description
+    # Define the request parameters based on the selected variable
+    if variable == "2m_temperature":
+        request_params = {
+            "variable": "2m_temperature",
+            "product_type": "reanalysis",
+            "year": year,
+            "month": "01",
+            "day": "01",
+            "time": "00:00",
+            "format": "netcdf",
+        }
+    elif variable == "2m_dewpoint_temperature":
+        # Define parameters for dew point temperature
+        # Add more cases for other variables
+        pass
+    else:
+        st.error("Selected variable is not supported.")
+        return None
 
-# Access variables
-alpha = nc.variables["t2m"][:]
-lat = nc.variables["latitude"][:]
-lon = nc.variables["longitude"][:]
-time = nc.variables["time"][:]
-time_units = nc.variables["time"].units 
+    # Download the data
+    with st.spinner(f"Downloading {variable} data for year {year}..."):
+        r = c.retrieve("reanalysis-era5-single-levels", request_params)
+        r.download("data.nc")
 
-# Fill missing values with 0
-alpha = alpha.filled(fill_value=0)
+    st.success(f"{variable} data for year {year} downloaded successfully.")
+    return "data.nc"
 
-def convert_time(t):
-    origin_date = pd.to_datetime(time_units.split("since")[-1].strip())  # Extract and parse the origin date
-    new_time = origin_date + pd.to_timedelta(t, unit=time_units.split("since")[0].strip())
-    return new_time
+# Streamlit UI components
+with st.form("data_fetch_api"):
+    col1, col2 = st.columns(2)
+    
+    # Select year
+    year = st.selectbox("Select Year", list(range(1979, datetime.now().year + 1)))
 
-# Function to format time for display
-def id_to_time(i):
-    return convert_time(time[i])
+    # Select variable
+    params = st.selectbox("Select Weather Parameter", ["2m_temperature", "2m_dewpoint_temperature"])
+    
+    st.write("Click on map to select location")
+    DEFAULT_LATITUDE = 51.96
+    DEFAULT_LONGITUDE = 7.62
+    m = folium.Map(location=[DEFAULT_LATITUDE, DEFAULT_LONGITUDE], zoom_start=8)
+    m.add_child(folium.LatLngPopup())
+    f_map = st_folium(m, width=670, height=500)
 
-# Cache Plotly figure creation
-@st.cache_data
-def plotly_plot(time_idx):
-    fig = go.Figure(data=go.Heatmap(x=lon, y=lat, z=alpha[time_idx], colorscale='RdBu_r'))
-    fig.update_xaxes(title_text='Longitude')
-    fig.update_yaxes(title_text='Latitude')
-    fig.update_layout(width=800, height=600)
-    return fig
-# Slider to select a specific date
-time_id = st.select_slider("Date", options=range(len(time)), format_func=id_to_time)
-# Create the Plotly figure and display it
-fig = plotly_plot(time_id)
-st.write(fig)
+    selected_latitude = DEFAULT_LATITUDE
+    selected_longitude = DEFAULT_LONGITUDE
+
+    if f_map.get("last_clicked"):
+        selected_latitude = f_map["last_clicked"]["lat"]
+        selected_longitude = f_map["last_clicked"]["lng"]
+
+    submitted = st.form_submit_button("Submit")
+
+if submitted:
+    st.success(f"Selected location: {selected_latitude}, {selected_longitude}")
+
+    # Download data based on user's selection
+    nc_file = download_data(year, params)
+    
+    if nc_file:
+        # Open the downloaded NetCDF file using xarray
+        ds = xr.open_dataset(nc_file)
+
+        # Visualize the data as desired
+        st.write(f"Visualization of {params} data for year {year} here.")
+        # You can create plots, maps, or other visualizations using the data from 'ds'
+
+        # Close the NetCDF file
+        ds.close()
