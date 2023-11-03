d982fdd1b9d6f4c0938fccb632033fdd3ddf9b88
Shamzmohamed <m20210707@novaims.unl.pt>
2023-11-03 21:22:50 +0100

    test file 2

--- test2.py
+++ test2.py
@@ -1,15 +1,20 @@
-# Total precipitation added
 import streamlit as st
 from datetime import datetime, timedelta
-from streamlit_folium import st_folium
+from streamlit_folium import st_folium, folium_static
 import folium
+from affine import Affine
+import rasterio
 import plotly.express as px
+import geopandas as gpd
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
 
 from climate_data_store_connection import ClimateDataStoreConnection
 
 st.set_page_config(page_title="CDS API", page_icon="🌤️")
 
-st.title("CDS-API Connection")
+st.title("ClimateDataStoreConnection")
 
 with st.form("data_fetch_api"):
 
@@ -17,11 +22,13 @@ with st.form("data_fetch_api"):
 
     params = st.multiselect(
         'Select Weather Parameters', help="CDS offers many parameter but few selected for demo purpose.",
-        options=['Ambient Temperature (°C)', 'Dew Point Temperature (°C)', 'Surface Pressure (kPa)', 'Total Precipitation (mm)'])
-    date = st.date_input('Select a date range',
-                         value=[datetime(datetime.today().year, 1, 1), datetime(datetime.today().year, 12, 31)],
-                         min_value=datetime(datetime.today().year, 1, 1),
-                         max_value=datetime(datetime.today().year, 12, 31))
+        options=['Ambient Temperature (°C)', 'Dew Point Temperature (°C)', 'Surface Pressure (kPa)','Total Precipitation (mm)'])
+
+    date = st.date_input('Select a date',
+                         value=datetime.today() - timedelta(days=6),
+                         min_value=datetime.fromisoformat('2023-01-01'),
+						 max_value=datetime.today() - timedelta(days=6))
+    
     st.write("Click on map to select location")
     
     # https://franekjemiolo.pl/using-selected-position-map-form-streamlit/
@@ -39,80 +46,92 @@ with st.form("data_fetch_api"):
     selected_latitude = DEFAULT_LATITUDE
     selected_longitude = DEFAULT_LONGITUDE
 
+    if f_map.get("last_clicked"):
+        selected_latitude = f_map["last_clicked"]["lat"]
+        selected_longitude = f_map["last_clicked"]["lng"]
+
     submitted = st.form_submit_button("Submit")
-
+    
 if submitted:
+
     st.success(f"Selected location: {selected_latitude}, {selected_longitude}")
-    weather_map = folium.Map(location=[selected_latitude, selected_longitude], zoom_start=8)
 
-    temperature_group = folium.FeatureGroup(name="Temperature")
-    precipitation_group = folium.FeatureGroup(name="Precipitation")
+    if params:            
 
-    if params:  
+        # API understands these names for input
         param_mapping_ip = {
-            "Ambient Temperature (°C)": "2m_temperature", "Dew Point Temperature (°C)": "2m_dewpoint_temperature",
-            "Surface Pressure (kPa)": "surface_pressure", "Total Precipitation (mm)": "total_precipitation"}
+            "Ambient Temperature (°C)":"2m_temperature",
+            "Dew Point Temperature (°C)": "2m_dewpoint_temperature",
+            "Surface Pressure (kPa)":"surface_pressure",
+            "Total Precipitation (mm)":"total_precipitation"} #tp works fine
 
+        # API returns these names in output
         param_mapping_op = {
             "t2m": "Ambient Temperature (°C)",
             "d2m": "Dew Point Temperature (°C)",
             "sp": "Surface Pressure (kPa)",
-            "tp": "Total Precipitation (mm)"
-        }
-        start_date, end_date = date
+            "tp":"Total Precipitation (mm)"}
+
+        # Preparing dictionary to send to API
         data = {
-            'product_type': 'reanalysis',
-            'format': 'netcdf',
-            'variable': [param_mapping_ip[p]
-                for p in params
-            ],
-            'year': [str(start_date.year)],
-            'month': [str(start_date.month)],
-            'day': [str(start_date.day)],
-            'time': [
-                '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
-                '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
-                '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
-                '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
-            ],
-            'area': [
-                selected_latitude, selected_longitude,
-                selected_latitude, selected_longitude,
-            ],
-        }
+                'product_type': 'reanalysis',
+                'format': 'netcdf',
 
+                'variable': [param_mapping_ip[p] for p in params],
+                'year': [str(date.year),],
+                'month': [str(date.month),],
+                'day': [str(date.day),],
+                'time': [
+                    '00:00', '01:00', '02:00',
+                    '03:00', '04:00', '05:00',
+                    '06:00', '07:00', '08:00',
+                    '09:00', '10:00', '11:00',
+                    '12:00', '13:00', '14:00',
+                    '15:00', '16:00', '17:00',
+                    '18:00', '19:00', '20:00',
+                    '21:00', '22:00', '23:00',],}
+                #'area': [selected_latitude, selected_longitude, selected_latitude,selected_longitude,],}
         with st.spinner("Fetching weather data..."):
-            full_year_data = None
 
-        # Iterate through each day of the year and fetch data
-        current_date = start_date
-        while current_date <= end_date:
-            data['year'] = [str(current_date.year)]
-        data['month'] = [str(current_date.month)]
-        data['day'] = [str(current_date.day)]
+            # Connection to API
+            conn = st.experimental_connection("", type = ClimateDataStoreConnection)
+            nc_data_path = conn.query(data, "")
+            print(data)
 
-        conn = st.experimental_connection("", type = ClimateDataStoreConnection)
-        day_data = conn.query(data)
+            st.title("ERA5 2m Temperature Data on Map")
+            latitude = 51.96
+            longitude = 7.62
+            def visualize_ERA5_data_on_map(nc_data_path, latitude, longitude):
+                data = xr.open_dataset(nc_data_path)
 
-        # Append the data to the full_year_data DataFrame
-        if full_year_data is None:
-            full_year_data = day_data
-        else :
-            full_year_data = full_year_data.append(day_data)
+            st.title("ERA5 2m Temperature Data on Map")
+            for var in data.data_vars:
+                print(f"Variable: {var}")
+                print(f"Attributes: {data[var].attrs}")
 
-        # Move to the next day
-        current_date += timedelta(days = 1)
+            # Create a Folium map
+            m = folium.Map(location = [latitude, longitude], zoom_start = 8)
 
-        # Plot the data for each selected parameter
-        for param in params:
-            param_name = param_mapping_ip[param]
-        st.write(f"Time series plot for {param} throughout the year")
+            # You can customize the visualization based on your requirements
+            st.write("Data Information:")
+            st.write(data)
 
-        if param_name in full_year_data.columns:
-            fig = px.line(x=full_year_data.index, y = full_year_data[param_name], labels = {
-                    'x': 'Time',
-                    'y': param},title = f"Time series plot of {param}")
-            st.plotly_chart(fig, use_container_width = True)
-        else :
-            st.error(f"Data for {param} is not available for the selected location throughout the year.")
+            # Add a marker to the map
+            folium.Marker([latitude, longitude], tooltip = 'Selected Location').add_to(m)
+            colormap = plt.get_cmap('coolwarm')# Adjust the colormap as needed
+            temperature_data = data['t2m'].isel(time = 0).values
+            normalized_temperature_data = (temperature_data - temperature_data.min()) / (temperature_data.max() - temperature_data.min())
+            colorized_temperature_data = colormap(normalized_temperature_data)
+            folium.raster_layers.ImageOverlay(
+                image = colorized_temperature_data,
+                bounds = [
+                    [data['latitude'].min(), data['longitude'].min()],
+                    [data['latitude'].max(), data['longitude'].max()]
+                ],
+                origin = 'lower',colormap = colormap).add_to(m)
+            st.write("Folium Map with ERA5 Data Layer")
+            folium_static(m)
+            visualize_ERA5_data_on_map(nc_data_path, latitude, longitude)
 
+    else:
+        st.error("Select minimum 1 weather parameter")
