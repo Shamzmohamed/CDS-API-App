f8cfb12d27b381420c339fae35ce72802147d436
Mohamed Shamsudeen <92031178+Shamzmohamed@users.noreply.github.com>
2024-02-20 21:55:15 +0100

    Update visual.py

--- visual.py
+++ visual.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+import numpy as np 
 import plotly.graph_objects as go
 import plotly.express as px
 import streamlit as st
